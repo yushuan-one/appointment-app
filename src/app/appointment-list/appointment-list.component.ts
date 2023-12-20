a6fbd823e42934db2edea8d7e6f6c6852bf21783
Xie Yuxuan <xie-yuxuan@gr.sei.co.jp>
2023-12-20 14:11:43 +0900

    removing appointments

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css']
+})
+export class AppointmentListComponent {
+  newAppointmentTitle: string = "";
+  newAppointmentDate: Date = new Date();
+  appointments: Appointment[] = [];
+
+  addAppointment() {
+    if(this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+
+      this.appointments.push(newAppointment);
+      this.newAppointmentTitle = "";
+      this.newAppointmentDate = new Date();
+
+    } 
+  }
+}
